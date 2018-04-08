802cb080cff0781c5379a0c36198d9dac5b9304a
NimaHRaja <nimaradja@gmail.com>
2018-04-08 23:10:10 +0100

    added init.r

---
+++ INIT.R
@@ -0,0 +1,19 @@
+# Libraries
+
+library(exifr)
+library(Rcpp)
+library(reshape2)
+library(ggplot2)
+
+
+# Source functions
+
+source("Get_list_of_Files.R")
+source("Get_Meta_for_a_Bucket.R")
+
+
+# Create directories
+
+if(!dir.exists("data")) {dir.create("data")}
+if(!dir.exists("data/list")) {dir.create("data/list")}
+if(!dir.exists("data/photo_meta")) {dir.create("data/photo_meta")}
